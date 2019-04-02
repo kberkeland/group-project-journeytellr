0b46453f6ba3c122371288ffb92ddad7f432907d
Kye Berkeland <kyeberkeland@yahoo.com>
2019-04-01 21:24:36 -0500

    Remove dummy data button

--- src/components/RegisterPage/UserInfo.js
+++ src/components/RegisterPage/UserInfo.js
@@ -33,15 +33,6 @@ class UserInfo extends Component {
     }
   } // end registerUser
 
-  dummy = () => {
-    let dummyProfile = { type: 'UPDATE_REGISTRATION', payload: {  email: 'mattkleven@gmail.com',
-                                                                  confirm_email: 'mattkleven@gmail.com',
-                                                                  password: 'a',
-                                                                  confirm_password: 'a',
-                                                                  profile_pic: 'https://journey-tellr-images.s3.amazonaws.com/bucketFolder/1553549316156-lg.jpg', }}
-    this.props.dispatch(dummyProfile);
-  }
-
   render() {
     const { registration,
       handleInputChangeFor,
@@ -104,7 +95,7 @@ class UserInfo extends Component {
             </label>
           </Col>
 
-          <Col span={8}>
+          <Col span={10}>
             <Button
               onClick={handleRegisterNavButton.bind(this, 'profile')}
               className='registration-button'>
@@ -112,14 +103,7 @@ class UserInfo extends Component {
               Back
             </Button>
           </Col>
-          <Col span={2}>
-          <Button
-              onClick={this.dummy}
-              style={{opacity: 0,}}>
-              Dummy
-            </Button>
-          </Col>
-          <Col span={8}>
+          <Col span={10}>
             <Button onClick={this.registerUser}
               type="primary"
               htmlType='submit'
